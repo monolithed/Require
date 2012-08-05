32962e904210a4e5767d1a00718e799679d5d10f
Alexander Guinness <monolithed@gmail.com>
2012-08-05 09:36:05 +0600

    removed minification staff

--- test/main.cpp
+++ test/main.cpp
@@ -6,9 +6,6 @@ int main()
 {
 	Require require;
 
-	// File path
-	std::string path("./files/");
-
 	// File names
 	std::string name("file_1.js;file_2.js;");
 
@@ -16,21 +13,21 @@ int main()
 	std::list<std::string> file;
 	require.split(name, ';', std::back_inserter(file));
 
-	/*
-	Or you can add the file names explicity:
-	file.push_back("file_1.js");
-	file.push_back("file_2.js");
-	*/
+	/**
+	 * // Or you can add the file names explicity:
+	 * file.push_back("file_1.js");
+	 * file.push_back("file_2.js");
+	**/
+
+	// File path
+	std::string path("./files/");
 
 	// load the file
 	if (require.load(file, path))
 	{
-		// Get the data
-		std::string data(require.data(true /* Optional minification */));
-
 		// Save data to the file
 		if (require.save(path + "file.js"))
-			std::cout << data << std::endl;
+			std::cout << require.data << std::endl;
 
 		// Adding data to the end of a File
 		//require.save(path + "./file.js", std::ios::app)
